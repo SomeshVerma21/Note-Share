e2f09edfee5f7a831104f46c6f17d16a5b2c33bf
Somesh <someshiet@gmail.com>
2022-06-09 23:56:10 +0530

    updated details page with comment section

--- app/src/main/java/com/vermaji/noteshare/mainUI/home/homeScreen/HomeItemAdapter.kt
+++ app/src/main/java/com/vermaji/noteshare/mainUI/home/homeScreen/HomeItemAdapter.kt
@@ -1,46 +1,40 @@
 package com.vermaji.noteshare.mainUI.home
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.vermaji.noteshare.R
 import com.vermaji.noteshare.databinding.NoteItemViewBinding
 import com.vermaji.noteshare.mainUI.home.homeScreen.models.Data
 import com.vermaji.noteshare.mainUI.home.homeScreen.models.NoteResponse
+import com.vermaji.noteshare.mainUI.home.notesDetails.NoteDetailsActivity
 
-class HomeItemAdapter(private val responseData:NoteResponse,private val clickListener: HomeItemListener)
+class HomeItemAdapter(val context:Context, private val responseData:NoteResponse,private val clickListener: HomeItemListener)
     : RecyclerView.Adapter<HomeItemAdapter.NoteItemViewHolder>() {
     private val list = responseData.list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteItemViewHolder
     {
-        return NoteItemViewHolder.getViewHolder(parent)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item_,parent,false)
+        return NoteItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: NoteItemViewHolder, position: Int)
     {
-        holder.bind(list[position],clickListener)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context,NoteDetailsActivity::class.java)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
-    class NoteItemViewHolder private constructor(private val binding: NoteItemViewBinding): RecyclerView.ViewHolder(binding.root)
+    class NoteItemViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView)
     {
-        fun bind(item: Data, clickListener: HomeItemListener)
-        {
-            binding.idNoteDownloadCount.text = item.downloads.toString()+" Downloads"
-            binding.clickListener = clickListener
-            binding.notesData = item
-            binding.executePendingBindings()
-        }
-        companion object
-        {
-            fun getViewHolder(parent: ViewGroup): NoteItemViewHolder
-            {
-                val layoutInflater = LayoutInflater.from(parent.context)
-                val view = NoteItemViewBinding.inflate(layoutInflater,parent,false)
-                return NoteItemViewHolder(view)
-            }
-        }
+
     }
 }
 class HomeItemListener(val clickListener:(itemId:Long)->Unit)
--- app/src/main/java/com/vermaji/noteshare/mainUI/home/notesDetails/NoteDetailsActivity.kt
+++ app/src/main/java/com/vermaji/noteshare/mainUI/home/notesDetails/NoteDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.vermaji.noteshare.mainUI.home.notesDetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentManager
 import com.vermaji.noteshare.R
 
@@ -11,9 +12,19 @@ class NoteDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_details)
+        supportActionBar?.hide()
         fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction()
             .add(R.id.fg_note_details,NoteDetailsFragment.newInstance())
             .commitNow()
+
+        initViews()
+    }
+
+    private fun initViews(){
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
