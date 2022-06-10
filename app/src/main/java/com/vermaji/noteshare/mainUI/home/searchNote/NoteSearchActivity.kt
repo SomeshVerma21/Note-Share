7681ddec74d692162e520cb5d363aa5d419fedca
somesh <somesh.verma@career360.com>
2022-06-10 17:29:46 +0530

    created search activty

---
+++ app/src/main/java/com/vermaji/noteshare/mainUI/home/searchNote/NoteSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.vermaji.noteshare.mainUI.home.searchNote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vermaji.noteshare.R
+
+class NoteSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_search)
+    }
+}
