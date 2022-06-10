c2139635bf32f1f1fa026645a32a762eed5613e5
hang_up <pk1114585>
2022-06-10 22:24:15 +0530

    demo try

--- app/src/main/java/com/vermaji/noteshare/network/Endpoints.kt
+++ app/src/main/java/com/vermaji/noteshare/network/Endpoints.kt
@@ -2,7 +2,7 @@ package com.vermaji.noteshare.network
 
 class Endpoints {
     companion object{
-        const val baseurl = "http://192.168.143.67:8080"
+        const val baseurl = "http://192.168.185.66:8080"
 
         const val loginUser = "/api/v1/user/login"
         const val registerUser = "/api/v1/user/register"
